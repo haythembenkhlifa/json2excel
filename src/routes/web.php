3746fba4d650dc90d796d88f6617a31b8ac6896f
Haythem Ben Khlifa <39649249+haythembenkhlifa@users.noreply.github.com>
2020-01-03 10:01:44 +0200

    Update web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -12,7 +12,7 @@ use haythem\json2excel\functionality\JsonToExcelMerge;
 
 
 
-Route::get('test', function () {
+/*Route::get('test', function () {
     $json = file_get_contents(__DIR__ . "\sample.json");
     $file_xlsx = Storage::disk('json2excel')->get('test.xlsx');
     $file_csv = Storage::disk('json2excel')->get('csv.csv');
@@ -23,3 +23,4 @@ Route::get('test', function () {
     //return $jsontoexcelmerge->mergeToCsv($json, $file_csv);
     //return $jsontoexcelmerge->mergeToExcel($json, $file_xlsx, 1, "xlsx");
 });
+*/
